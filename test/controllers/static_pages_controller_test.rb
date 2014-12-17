db00b5b2731fbdb72590ee12384e4f28aae1c51f
PalermoD <palermo.deschamps@gmail.com>
2014-12-17 11:43:01 -0500

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Gotham24 New York Social"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Gotham24 New York Social"
   end
 
-end
+  test "should get contact" do
+    get :contact
+  	assert_response :success
+  	assert_select "title", "Contact | Gotham24 New York Social"
+  end
+end 
