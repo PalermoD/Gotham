d96b4461fed5800d24edeb76104cb5c7fd71644a
PalermoD <palermo.deschamps@gmail.com>
2014-12-17 17:48:21 -0500

    made a basic user model(including passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+ 
+
+  get 'signup' => 'users#new'
+
   root 'static_pages#home'
 
-  get   'about' => 'static_pages#about'
+  get 'about' => 'static_pages#about'
 
   get 'contact' => 'static_pages#contact'
 
