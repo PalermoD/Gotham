1389d89085269cbc1b9d6d1d11747596b0e3af28
PalermoD <palermo.deschamps@gmail.com>
2014-12-18 11:03:46 -0500

    finished log in and log out fuctionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
 
+  get 'sessions/new'
+
   get 'signup' => 'users#new'
 
   root 'static_pages#home'
@@ -8,6 +10,12 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
 
   get 'contact' => 'static_pages#contact'
+
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 end
